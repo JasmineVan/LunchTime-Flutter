7930b99089276c6306d5305a8399ab52001f8026
TrinhThuong2001 <88668726+JasmineVan@users.noreply.github.com>
2023-09-28 16:03:14 +0700

    Adjust Random Food screen

--- lib/pages/random_food.dart
+++ lib/pages/random_food.dart
@@ -17,18 +17,76 @@ class RandomFood extends StatefulWidget {
 class _RandomFoodState extends State<RandomFood> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xFFFFD18F),
-      alignment: Alignment.center,
-      child: ElevatedButton(
-        child: const Text('Choose food'),
-        onPressed: () {
-          final dummyData = DummyData();
-          final random = Random();
-          int randomIndex = random.nextInt(dummyData.listProduct.length);
-          Get.to(RandomFoodDetail(index: randomIndex, playConfetti: true,));
-        },
-      ),
-    );
+
+    double maxWidth = MediaQuery.of(context).size.width;
+    double maxHeight = MediaQuery.of(context).size.height;
+
+     return Scaffold(
+       backgroundColor: const Color(0xFFF9DEC9),
+       body: Center(
+         child: Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           crossAxisAlignment: CrossAxisAlignment.center,
+           children: [
+             Container(
+               width: maxWidth/2 + maxWidth/4,
+               height: maxHeight/2 + maxWidth/4,
+               decoration: const BoxDecoration(
+                 shape: BoxShape.circle,
+                 color: Color(0xFFD80032),
+               ),
+               child: Column(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 crossAxisAlignment: CrossAxisAlignment.center,
+                 children: [
+                   Container(
+                     width: maxWidth/2,
+                     height: maxHeight/2,
+                     decoration: const BoxDecoration(
+                       shape: BoxShape.circle,
+                       color: Color(0xFFF78CA2),
+                     ),
+                     child: Column(
+                       mainAxisAlignment: MainAxisAlignment.center,
+                       crossAxisAlignment: CrossAxisAlignment.center,
+                       children: [
+                         Container(
+                           width: maxWidth/4,
+                           height: maxHeight/4,
+                           decoration: const BoxDecoration(
+                             shape: BoxShape.circle,
+                             color: Color(0xFFF9DEC9),
+                           ),
+                           alignment: Alignment.center,
+                           child: SizedBox(
+                               width: maxWidth/6,
+                               height: 40.0,
+                               child: ElevatedButton(
+                                   style: ElevatedButton.styleFrom(
+                                     backgroundColor: const Color(0xFF3D0C11),
+                                   ),
+                                   onPressed: () {
+                                     final dummyData = DummyData();
+                                     final random = Random();
+                                     int randomIndex = random.nextInt(dummyData.listProduct.length);
+                                     Get.to(RandomFoodDetail(index: randomIndex, playConfetti: true,));
+                                   },
+                                   child: const Text(
+                                     'Food',
+                                     style: TextStyle(
+                                       color: Colors.white,
+                                     ),
+                                   ))),
+                         ),
+                       ],
+                     ),
+                   ),
+                 ],
+               ),
+             ),
+           ],
+         ),
+       ),
+     );
   }
 }
